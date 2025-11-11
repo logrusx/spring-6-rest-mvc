ae224c09e6a621ae8a2e3f32487532bcea669f29
logrusx <jorodrbr@yahoo.com>
2025-11-11 18:15:41 +0200

    Externalize base URL property

--- src/main/java/guru/springframework/spring6resttemplate/client/config/RestTemplateBuilderConfig.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/config/RestTemplateBuilderConfig.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6resttemplate.client.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.web.client.RestTemplateBuilderConfigurer;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,10 +10,13 @@ import org.springframework.web.util.DefaultUriBuilderFactory;
 @Configuration
 public class RestTemplateBuilderConfig {
 
+    @Value("${rest.template.baseUrl}")
+    String baseUrl;
+
     @Bean
     RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer) {
         RestTemplateBuilder builder = configurer.configure(new RestTemplateBuilder());
-        DefaultUriBuilderFactory uriBuilderFactory = new DefaultUriBuilderFactory("https://localhost:8080");
-        return  builder.uriTemplateHandler(uriBuilderFactory);
+        DefaultUriBuilderFactory uriBuilderFactory = new DefaultUriBuilderFactory(baseUrl);
+        return builder.uriTemplateHandler(uriBuilderFactory);
     }
 }
