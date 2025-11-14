95e85b37036edeeb42fa0080da7b916ecd42e23c
logrusx <jorodrbr@yahoo.com>
2025-11-14 21:25:23 +0200

    Client implementation and tests

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -1,11 +1,28 @@
 package guru.springframework.spring6resttemplate.client;
 
 import guru.springframework.spring6resttemplate.model.BeerDTO;
+import guru.springframework.spring6resttemplate.model.BeerDTOPageImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+@RequiredArgsConstructor
+@Service
 public class BeerClientImpl implements BeerClient {
+
+    public static final String GET_BEER_PATH = "/api/v1/beer";
+    private final RestTemplateBuilder restTemplateBuilder;
+
     @Override
     public Page<BeerDTO> listBeers() {
-        return null;
+        RestTemplate restTemplate = restTemplateBuilder.build();
+
+        ResponseEntity<BeerDTOPageImpl> response =
+                restTemplate.getForEntity(GET_BEER_PATH, BeerDTOPageImpl.class);
+
+        return response.getBody();
     }
 }
